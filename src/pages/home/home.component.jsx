a3d57b7e520191ce50203f5ab963b5559aac8ccc
karismapa <karisma.pratama@manyoption.co.id>
2020-12-29 09:18:18 +0700

    start working on shop page

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import './home.styles.scss';
 
 const HomePage = () => (
-  <div className='homepage'>
+  <div className='home-page'>
     <Directory />
   </div>
 )
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import ShopData from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: ShopData,
+    }
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {
+          collections.map(collection => (
+            <CollectionPreview
+              key={collection.id}
+              title={collection.title}
+              items={collection.items}
+            />
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default ShopPage;
