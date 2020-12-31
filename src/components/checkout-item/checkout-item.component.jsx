17446531faaa49bdfc74c75aea7ee8faa0c7f81e
karismapa <karisma.pratama@manyoption.co.id>
2020-12-31 12:31:20 +0700

    add substractItem functionality and add addItem and substractItem to checkout-item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addItem, substractItem, clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, qty } }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={imageUrl} alt='item' />
-    </div>
-    <span className='name'>{name}</span>
-    <span className='qty'>{qty}</span>
-    <span className='price'>{price}</span>
-    <div className='remove-button'>&#10005;</div>
-  </div>
-)
+const CheckoutItem = ({ cartItem, addItem, substractItem, clearItem }) => {
+  const { name, imageUrl, price, qty } = cartItem
 
-export default CheckoutItem;
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img src={imageUrl} alt='item' />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='qty'>
+        <div
+          className='arrow'
+          onClick={() => substractItem(cartItem)}
+        >&#10094;</div>
+        <span className='value'>{qty}</span>
+        <div
+          className='arrow'
+          onClick={() => addItem(cartItem)
+        }>&#10095;</div>
+      </span>
+      <span className='price'> {price} </span>
+      <div
+        className='remove-button'
+        onClick={() => clearItem(cartItem)}
+      >&#10005;</div>
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+  substractItem: item => dispatch(substractItem(item)),
+  clearItem: item => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, qty: 1 }]
 }
+
+export const substractItemFromCart = (cartItems, cartItemToSubstract) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToSubstract.id
+  )
+
+  if (existingCartItem) {
+    if (existingCartItem.qty === 1) {
+      return cartItems.filter(cartItem => cartItem.id !== cartItemToSubstract.id);
+    }
+
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToSubstract.id
+        ? { ...cartItem, qty: cartItem.qty - 1 }
+        : cartItem
+    )
+  }
+
+  return cartItems
+}
