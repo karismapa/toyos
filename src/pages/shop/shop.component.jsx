07dfc93d93b3777e17530390598f98d6c88c8109
karismapa <karisma.pratama@manyoption.co.id>
2021-01-01 07:13:15 +0700

    store cart data to local storage with redux-persist and move shop data to redux reducer

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
+
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    {
+      collections.map(({ id, ...otherProps }) => (
+        <CollectionPreview key={id} { ...otherProps } />
+      ))
+    }
+  </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+})
+
+export default connect(mapStateToProps)(ShopPage);
